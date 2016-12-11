d72219bfebe3602e1400351d9bd5c1d6e6d86673
Timotej Gale <timotej.gale@gmail.com>
2016-12-11 18:21:00 +0100

    Add app for automatic file deletion

--- FRIm/settings.py
+++ FRIm/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
-    'documents.apps.DocumentsConfig'
+    'documents.apps.DocumentsConfig',
+    'django_cleanup',
 ]
 
 MIDDLEWARE = [
