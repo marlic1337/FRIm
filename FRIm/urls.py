c045263310e00e2079ec63845274ef9988649290
Timotej Gale <timotej.gale@gmail.com>
2016-12-11 13:10:47 +0100

    Include 'documents' app in project

--- FRIm/urls.py
+++ FRIm/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^', include('home.urls')),
+    url(r'^documents/', include('documents.urls')),
     url(r'^admin/', admin.site.urls),
 ]
