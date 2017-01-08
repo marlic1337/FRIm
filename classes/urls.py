6be5b8ed0bf9d7034a1699573912536e32872d7c
Mark Urh Rebec <mark.urhrebec@gmail.com>
2017-01-08 14:13:17 +0100

    Project remake

--- classes/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-app_name = 'classes'
-
-urlpatterns = [
-    # /classes/
-    url(r'^$', views.index, name='index'),
-]
--- forum/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-#def index(request):
-#    return HttpResponse("<h2>Test</h2>")
-
-def index(request):
-    return render(request, 'forum/home.html')
