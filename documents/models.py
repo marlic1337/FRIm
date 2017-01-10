f3d4e871ca7e24e083f55ceb05d2896f29b329a8
Timotej Gale <timotej.gale@gmail.com>
2017-01-10 16:53:31 +0100

    [documents] Fix migrations

--- documents/models.py
+++ documents/models.py
@@ -8,5 +8,5 @@ class Document(models.Model):
     title = models.CharField(max_length=60)
     description = models.CharField(max_length=1000)
     date = models.DateTimeField(auto_now_add=True)
-    owner = models.ForeignKey('User.CustomUser', on_delete=models.CASCADE)
-    subject = models.ForeignKey('classes.Predmet', on_delete=models.CASCADE)
+    owner = models.ForeignKey('User.CustomUser', null=True, on_delete=models.CASCADE)
+    subject = models.ForeignKey('classes.Predmet', null=True, on_delete=models.CASCADE)
