79031c13b50e477849e820c563e7daa715cc0bb5
Miha <kamensek.miha@gmail.com>
2017-01-10 02:28:06 +0100

    Merge remote-tracking branch 'refs/remotes/origin/master'
    Conflicts:
    	FRIm/urls.py

--- FRIm/settings.py
+++ FRIm/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'FRIm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -136,7 +136,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Settings for sending emails
 
--- FRIm/urls.py
+++ FRIm/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^', include('home.urls')),
@@ -22,4 +24,4 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^classes/', include('classes.urls')),
     url(r'^forum/', include('forum.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
