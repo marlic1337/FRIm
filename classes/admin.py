1019e0b5f4a15759d22735d2046d3af55ea86a1a
Mark Urh Rebec <mark.urhrebec@gmail.com>
2017-01-11 22:33:47 +0100

    documents fix

--- classes/admin.py
+++ classes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Urnik, Profesor, Prostor, Predmet, Skupina
+
+admin.site.register(Urnik)
+admin.site.register(Profesor)
+admin.site.register(Prostor)
+admin.site.register(Predmet)
+admin.site.register(Skupina)
